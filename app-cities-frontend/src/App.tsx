e85e32bdf91814966a96beaa981e7e0767a45b1e
Vadim Frolov <vadimf74@ya.ru>
2025-03-12 22:42:41 +0100

    APP style

--- app-cities-frontend/src/App.tsx
+++ app-cities-frontend/src/App.tsx
@@ -4,9 +4,6 @@ import { CityList } from './components/CityList'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>City Directory</h1>
-      </header>
       <main>
         <CityList />
       </main>
